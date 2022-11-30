2964c38e7414d8cc0fa208717c97a0fdcb6054c4
FurWaz <fur.waz06@gmail.com>
2022-11-30 13:33:09 +0100

    Changed colors for better readability

--- src/front/scripts/header.js
+++ src/front/scripts/header.js
@@ -3,13 +3,13 @@ import User from "./User.js";
 
 const HEADER_CONTENT = `<div class="flex grow h-fit rounded-lg shadow-xl bg-transparent border-2 border-pink-600 px-4 py-4">
 <div class="flex flex-col justify-center w-fit">
-    <a href="/index.html" class="text-2xl font-extrabold text-pink-600"> Cocktails & Cie </a>
+    <a href="/index.html" class="text-2xl font-extrabold text-slate-200"> Cocktails & Cie </a>
 </div>
 <div class="flex justify-center grow space-x-8">
-    <a class="text-xl font-semibold text-pink-600 px-2 rounded cursor-pointer
-              transition-all hover:bg-pink-600 hover:text-slate-700" href="/list.html"> Cocktails </a>
-    <a class="text-xl font-semibold text-pink-600 px-2 rounded cursor-pointer
-              transition-all hover:bg-pink-600 hover:text-slate-700" href="/favorites.html"> Favorites </a>
+    <a class="text-xl font-semibold text-slate-200 px-2 rounded cursor-pointer
+              transition-all hover:bg-pink-600" href="/list.html"> Cocktails </a>
+    <a class="text-xl font-semibold text-slate-200 px-2 rounded cursor-pointer
+              transition-all hover:bg-pink-600" href="/favorites.html"> Favorites </a>
 </div>
 <div class="flex justify-end">
     <div id="is-connected">
@@ -17,10 +17,10 @@ const HEADER_CONTENT = `<div class="flex grow h-fit rounded-lg shadow-xl bg-tran
         hover:border-pink-600 hover:text-slate-700 hover:bg-pink-600" href="/profile.html"> Mon profile </a>
     </div>
     <div id="is-disconnected" class="space-x-4">
-        <a class="border-2 border-transparent rounded px-2 bg-transparent text-pink-600 font-semibold transition-all
-              hover:text-slate-700 hover:bg-pink-600" href="/register.html"> S'inscrire </a>
-        <a class="border-2 border-pink-600 rounded px-2 bg-transparent text-pink-500 font-semibold transition-all
-              hover:border-pink-600 hover:text-slate-700 hover:bg-pink-600" href="/login.html"> Se connecter </a>
+        <a class="border-2 border-transparent rounded px-2 bg-transparent text-slate-200 font-semibold
+              transition-all hover:bg-pink-600" href="/register.html"> S'inscrire </a>
+        <a class="border-2 border-pink-600 rounded px-2 bg-transparent text-slate-200 font-semibold transition-all
+              hover:border-pink-600 hover:bg-pink-600" href="/login.html"> Se connecter </a>
     </div>
 </div>
 </div>`;

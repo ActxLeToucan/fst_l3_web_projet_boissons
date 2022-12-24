13e407d035d118744ff51057ce5f26909e1f5b2d
FurWaz <fur.waz06@gmail.com>
2022-12-24 13:22:22 +0100

    Comments

--- src/front/scripts/API.js
+++ src/front/scripts/API.js
@@ -1,53 +1,8 @@
-class Credentials {
-    static get TYPE() {
-        return {
-            UNKNOWN: 0,
-            TOKEN: 1,
-            CREDENTIALS: 2
-        };
-    }
-
-    static fromToken(token) {
-        return new Credentials({token: token, type: Credentials.TYPE.TOKEN});
-    }
-
-    static fromCredentials(username, password) {
-        return new Credentials({username: username, password: password, type: Credentials.TYPE.CREDENTIALS});
-    }
-
-    token = "";
-    username = "";
-    password = "";
-    type = Credentials.TYPE.UNKNOWN;
-
-    constructor(infos) {
-        this.token = infos.token ?? this.token;
-        this.username = infos.username ?? this.username;
-        this.password = infos.password ?? this.password;
-        this.type = infos.type ?? this.type;
-    }
-
-    isValid() {
-        return this.type != Credentials.TYPE.UNKNOWN;
-    }
-
-    getToken() {
-        return this.token;
-    }
-
-    getUsername() {
-        return this.username;
-    }
-
-    getPassword() {
-        return this.password;
-    }
-}
-
+/**
+ * Classe API, construite pour faciliter les requetes vers l'API
+ */
 class API {
-    static Credentials = Credentials;
-
-    // API constants
+    // Constantes pour la classe API (types de requetes, types de fichiers, etc...)
     static API_URL = '';
     static get METHOD() {
         return {
@@ -66,34 +21,26 @@ class API {
             NONE: undefined
         }
     }
-    static get AuthorizationHeader() { return "x-furwaz-auth"; };
-
-    // API routes
-    static ROUTE = {
-        LOGIN: "/auth/token",
-        RESET: "/auth/reset",
-        PASSWORD: "/auth/password",
-        ME: "/users/me",
-        USERS: "/users"
-    };
 
     /**
-     * Makes an API call with the specified parameters
-     * @param {string} path API call url path (see API.ROUTES for possible routes)
-     * @param {string} method API call method (see API.METHOD for possible values)
-     * @param {object|string} body API call body (data to send, ignored if METHOD.GET is used)
-     * @param {string} type API call data type (see API.TYPE for possible values))  
-     * @param {object[]}} headers API call additionnal headers
-     * @returns a promise resolving when the API call is done
+     * Fais un appel a l'API avec les parametres passes en parametres
+     * @param {string} path Route de l'API a appeler
+     * @param {string} method Methode HTTP a utiliser (voir API.METHOD pour les valeurs possibles)
+     * @param {object|string} body Corps de la requete
+     * @param {string} type Type de la requete (voir API.TYPE pour les valeurs possibles)
+     * @param {object[]}} headers Headers a ajouter a la requete
+     * @returns {Promise} Promise de la requete
      */
     static execute(path, method = this.METHOD.GET, body = {}, type = this.TYPE.JSON, headers = {}) {
         return new Promise((resolve, reject) => {
+            // On nettoie le path et on y ajoute l'URL de l'API
             path = path.replace("/?", "?").replaceAll("//", "/");
             let urlparts = path.split("?");
             let base = urlparts.splice(0, 1);
             let params = (urlparts.length > 0)? ("?" + urlparts.join("&")) : "";
             path = base + params;
 
+            // On cree les headers de la requete
             let reqHeaders = {
                 "User-Agent": navigator.userAgent,
                 "Accept": "application/json",
@@ -101,10 +48,12 @@ class API {
                 "Accept-Language": "fr"
             };
 
+            // Si des headers sont passes en parametres, on les ajoute a la requete
             if (headers)
                 for (let key in headers)
                     reqHeaders[key] = headers[key];
 
+            // On créé le body de la requete depuis le body passe en parametres (ca peut etre un string, object, ...)
             let reqBody = {};
             if (body && type != this.TYPE.FILE) {
                 switch (typeof (body)) {
@@ -121,10 +70,12 @@ class API {
                 }
             }
 
+            // On ajoute l'URL de l'API si elle n'est pas definie
             if (API.API_URL == "" || API.API_URL == undefined) {
                 API.API_URL = window.location.origin + "/api";
             }
 
+            // Callback de gestion des erreurs
             const handleError = (err) => {
                 if (!err.json) reject(err);
                 else err.json().then(error => {
@@ -134,6 +85,7 @@ class API {
                 }).catch(error => reject(err));
             };
             
+            // On fait la requete a l'API
             fetch(API.API_URL + path, {
                 credentials: "omit",
                 method: method,
@@ -142,47 +94,58 @@ class API {
                 referrer: window.location.origin,
                 mode: "cors"
             }).then(response => {
+                //si la reponse n'est pas 200, on gere l'erreur
                 if (response.status != 200)
                     handleError(response);
                 else {
+                    // sinon on renvoie la reponse en json
                     response.json().then(data => {
                         resolve(data);
                     }).catch(err => handleError(err));
                 }
             }).catch(err => {
+                // erreur, on gere l'erreur
                 handleError(err);
             });
         });
     }
 
     /**
-     * Makes a logged API call with the specified parameters, using the specified credentials (token + token type / username + password)
-     * @param {string} path API call url path (see API.ROUTES for possible routes)
-     * @param {string} method API call method (see API.METHOD for possible values)
-     * @param {Credentials} credentials API call credentials to use (use User.currentUser.getCredentials() to get the current user's credentials)
-     * @param {object|string} body API call body (data to send, ignored if METHOD.GET is used)
-     * @param {string} type API call data type (see API.TYPE for possible values))
-     * @param {object[]} headers API call additionnal headers
-     * @returns A promise resolving when the API call is done
+     * Fais un appel a l'API avec les parametres passes en parametres, en utilisant le token de l'utilisateur
+     * @param {string} path Route de l'API a appeler
+     * @param {string} method Methode HTTP a utiliser (voir API.METHOD pour les valeurs possibles)
+     * @param {string} token Token de l'utilisateur
+     * @param {object|string} body Corps de la requete
+     * @param {string} type Type de la requete (voir API.TYPE pour les valeurs possibles)
      */
     static execute_logged(path, method = API.METHOD.GET, token, body = {}, type = this.TYPE.JSON) {
         return new Promise((resolve, reject) => {
+            // si le token n'est pas defini, on renvoie une erreur
             if (!token) {
                 reject({status: -1, message: "Please provide token"});
                 return;
             }
 
+            // Sinon on fais un appel classique avec le token en header
             this.execute(path, method, body, type, {token: token}).then(resolve).catch(reject);
         });
     }
 
+    /**
+     * Ajoute un parametre au lien donne
+     * @param {String} query lien de la requete
+     * @param {String} name nom du parametre
+     * @param {String} value valeur du parametre
+     * @returns le nouveau lien avec le parametre ajoute
+     */
     static createParam(query, name, value) {
+        // si le parametre n'est pas defini, on renvoie le lien sans modification
         if (value == undefined || value == null) return query;
         
+        // Sinon on ajoute le parametre au lien (si il en a deja, on ajoute un &, sinon on ajoute un ?)
         let hasParams = query.indexOf("?") != -1;
         return query + (hasParams ? "&" : "?") + name + "=" + value;
     }
 }
 
-window.API = API; // for debug purposes
 export default API;
--- src/front/scripts/admin.js
+++ src/front/scripts/admin.js
@@ -1,34 +1,46 @@
 import { initHeader } from "./header.js";
-import { checkForMigration } from "./favorites.js";
 import User from "./User.js";
 import API from "./API.js";
 import { setElementStyle } from "./common.js";
 
+// quand la page est chargée
 onload = () => {
     initHeader();
-    checkForMigration();
 
+    // si l'utilisateur n'est pas connecté ou n'est pas admin, on reviens a la page d'accueil
     if (!User.isConnected() || User.CurrentUser.level != 1) {
         window.location.href = window.location.origin;
         return;
     }
 
+    // sinon on lance la fonction setup
     setup();
 }
 
+/**
+ * Mets en place la page web (Evenements, etc...)
+ */
 function setup() {
+    // au click sur le bouton "charger la base de données", on lance la fonction loadBdd
     const load_bdd_btn = document.getElementById("load-bdd-btn");
     load_bdd_btn.addEventListener("click", ev => loadBdd());
 }
 
+/**
+ * Affiche un message a l'utilisateur
+ * @param {String} message message a afficher
+ */
 function log(message) {
+    // on recupere la zone des messages
     const log_zone = document.getElementById("log-zone");
 
+    // on cree une nouvelle popup message
     const p = document.createElement("p");
     setElementStyle(p, "popup-show text-slate-700 bg-pink-100 border-2 border-pink-600 rounded-lg p-2 shadow-lg m-2 w-fit mx-auto");
     p.innerText = message;
     log_zone.appendChild(p);
 
+    // au bout de 5sec on la supprime
     setTimeout(() => {
         p.classList.remove("popup-show");
         p.classList.add("popup-hide");
@@ -38,11 +50,17 @@ function log(message) {
     }, 5000);
 }
 
+/**
+ * Fais une requete au serveur pour charger la base de données
+ */
 function loadBdd() {
     log("Initialisation de la base de données...");
+    // on utilise la classe API pour faire la requete
     API.execute_logged("/db/init", API.METHOD.GET, User.CurrentUser.token).then(res => {
+        // si la requete a reussi, on affiche un message de succes
         log("Base de données initialisée !");
     }).catch(err => {
+        // si la requete a echoué, on affiche un message d'erreur
         if (typeof(err) == "string") {
             log("Erreur : " + err);
         } else {
